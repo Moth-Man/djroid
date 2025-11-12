00f78f82b68c88d0da0be4f3f7a4683769f99957
moth-man <mothmanor@gmail.com>
2025-11-11 18:38:38 -0700

    Reorganize textual gui into compartmentalized folders.

---
+++ djroid/gui/colors.py
@@ -0,0 +1,16 @@
+"""Color constants for row highlighting."""
+
+
+class HighlightColors:
+    """Color constants for row highlighting."""
+    WARNING_BG = "#3d2817"
+    WARNING_FG = "#ffb454"
+
+    ERROR_BG = "#3d1a1a"
+    ERROR_FG = "#ff6b6b"
+
+    SUCCESS_BG = "#1a3d2a"
+    SUCCESS_FG = "#5ffa7f"
+
+    INFO_BG = "#1a2a3d"
+    INFO_FG = "#5f9ffa"
